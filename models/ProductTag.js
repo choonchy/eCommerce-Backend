5197554a4af36658fb1bc044f80bfbf3d268f87c
choonchy <57583113+choonchy@users.noreply.github.com>
2021-08-12 21:19:18 +1000

    completed tasks

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,16 +5,27 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		product_id: {
+			type: DataTypes.INTEGER,
+		},
+		tag_id: {
+			type: DataTypes.INTEGER,
+		},
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'product_tag',
+	}
 );
 
 module.exports = ProductTag;
